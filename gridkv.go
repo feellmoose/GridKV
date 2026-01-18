2035af3d370e84c3503070e7df24c2f5695081db
feellmoose <feell4429@163.com>
2026-01-18 22:43:26 +0800

    chore: update version to v0.3.6

--- gridkv.go
+++ gridkv.go
@@ -22,7 +22,7 @@ import (
 )
 
 // Version represents the current version of GridKV
-const Version = "v0.3.5"
+const Version = "v0.3.6"
 
 // Error constants exported for application error handling
 var (
